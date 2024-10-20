205e9b89462fabc3a8e7aa4bbe1a2323bfe6165e
AreebAl <areebcalm@gmail.com>
2024-10-20 20:46:31 +0530

    port setup

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ app.register_blueprint(purchase_blueprint)  # Assuming you're using Flask bluepr
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host='0.0.0.0', port=5000)
